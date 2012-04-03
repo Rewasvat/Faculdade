89aa074713cf0ffa5a3a6013b73c9227a739a967
Rewasvat <fernando_aluani@hotmail.com>
2012-04-03 14:40:49 -0300

    adicionado arquivos de classes que certeza que irao existir; cmake e todo atualizado; vector3D implementado;

---
+++ MAC420_ComputacaoGrafica/EP1/src/vector3D.cc
@@ -0,0 +1,69 @@
+#include <math.h>
+#include <vector3D.h>
+
+namespace math {
+
+double Vector3D::Length() const {
+    return sqrt( (x*x) + (y*y) + (z*z) );
+}
+
+void Vector3D::Normalize() const {
+    double norm = Length();
+    if (norm != 0.0) {
+        x = x / norm;
+        y = y / norm;
+        z = z / norm;
+    }
+    else {
+        x = y = z = 0.0;
+    }
+}
+
+void Vector3D::Scale(const Vector3D &scale) const {
+    x *= scale.x;
+    y *= scale.y;
+    z *= scale.z;
+}
+
+Vector3D& Vector3D::operator+=(const Vector3D &other) {
+    x += other.x;  
+    y += other.y;
+    z += other.z;
+    return *this;
+}
+
+Vector3D& Vector3D::operator-=(const Vector3D &other) {
+    x -= other.x;
+    y -= other.y;
+    z -= other.z;
+    return *this;
+}
+
+Vector3D Vector3D::operator+(const Vector3D &right) const {
+    return Vector3D::Add(*this, right);
+}
+Vector3D Vector3D::operator-(const Vector3D &right) const {
+    return Vector3D::Subtract(*this, right);
+}
+Vector3D Vector3D::operator-() const {
+    return Vector3D(-this->x, -this->y, -this->z);
+}
+
+Vector3D Vector3D::operator*(const double &scalar) const {
+    return Vector3D::Multiply(*this, scalar);
+}
+Vector3D Vector3D::operator/(const double &scalar) const {
+    return Vector3D::Multiply(*this, 1.0 / scalar);
+}
+double Vector3D::operator*(const Vector3D &right) const {
+    return Vector3D::InnerProduct(*this, right);
+}
+Vector3D CrossProduct(const Vector3D &right) const {
+    return Vector3D::CrossProduct(*this, right);
+}
+
+Vector3D operator*(const double &scalar, const Vector3D &right) {
+    return Vector3D::Multiply(right, scalar);
+}
+
+}
---
+++ MAC420_ComputacaoGrafica/EP1/src/vector3D.h
@@ -0,0 +1,84 @@
+
+#ifndef VECTOR3D_H_
+#define VECTOR3D_H_
+
+namespace math {
+
+class Vector3D {
+  public:
+
+    Vector3D() : x(0.0), y(0.0), z(0.0) {}
+    Vector3D(double x, double y, double z) : x(x), y(y), z(z) {}
+
+    ~Vector3D() { }
+
+    // Fields are accessible as x/y/z or val[0], val[1] or val[2].
+    union {
+        struct { double x, y, z; };
+        struct { double val[3]; };
+    };
+
+    /// Returns the norm-2 of this vector
+    /** The norm-2 of a vector (x,y) is sqrt( x^2 + y^2 )    */
+    double Length() const;
+    
+    /// Returns a new vector equal to this vector normalized
+    /** The normalized vector (x,y) is (x,y) / Lenght(x,y)    */
+    void Normalize() const;
+
+    /// Returns a new vector which is this vector scaled coordinate by coordinate with "scale".
+    void Scale(const Vector3D &scale) const;
+
+    Vector3D& operator+=(const Vector3D &other);
+    Vector3D& operator-=(const Vector3D &other);
+
+    Vector3D operator+(const Vector3D &right) const; // operator to add
+    Vector3D operator-() const; // negation operator (return opposite vector)
+    Vector3D operator-(const Vector3D &right) const; // operator to subtract
+    Vector3D operator*(const double &scalar) const; // operator to scale
+    Vector3D operator/(const double &scalar) const; // operator to inverse scale (scale by 1.0/scalar)
+    double operator*(const Vector3D &right) const; // operator to inner product
+    Vector3D CrossProduct(const Vector3D &right) const; 
+    
+    // Static methods
+
+    /* Returns the sum of two vectors as a new vector. */
+    static Vector3D Add(const Vector3D &a, const Vector3D &b) {
+        return Vector3D(a.x + b.x, a.y + b.y, a.z + b.z);
+    }
+    
+    /* Subtracts the two vectors, returning a new one. */
+    static Vector3D Subtract(const Vector3D &a, const Vector3D &b) {
+        return Vector3D(a.x - b.x, a.y - b.y, a.z - b.z);
+    }
+    
+    /* Returns a new vector, equal to A scaled by the scalar value. */
+    static Vector3D Multiply(const Vector3D &a, const double &scalar) {
+        return Vector3D(a.x * scalar, a.y * scalar, a.z * scalar);
+    }
+    
+    /* Returns the inner product of two vectors A and B, which is directly related to the length and angle between the vectors.
+      So A * B = |A| * |B| * cos(angle between A and B) */
+    static double InnerProduct(const Vector3D &a, const Vector3D &b) {
+        return (a.x * b.x) + (a.y * b.y) + (a.z * b.z);
+    }
+    
+    /* Returns the cross product of two vectors A and B, which contrasts with the InnerProduct.
+       This returns a vector which is perpendicular to both A and B (the plane defined by A and B),
+       with a direction given by the right-hand rule and a magnitude equal to the area of the parallelogram
+       that A and B span (the area of the AB plane). */
+    static double CrossProduct(const Vector3D &a, const Vector3D &b) {
+        return Vector3D( (a.y * b.z) - (a.z * b.y),
+                         (a.z * b.x) - (a.x * b.z),
+                         (a.x * b.y) - (a.y * b.x) );
+    }
+};
+
+ /// Method that returns a vector equal to the a vector multiplied by a scalar
+    /** A vector (x,y) multiplied by a scalar a is equal to the vector (x * scalar, y*scalar)
+    *  @return Scalar Multiplied Vector
+    */
+Vector3D operator*(const double  &scalar, const Vector3D &right);
+
+} 
+#endif
