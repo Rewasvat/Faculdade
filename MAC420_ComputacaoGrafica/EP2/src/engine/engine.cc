1aac65180967bce664ca2e8ad1505321aed9eaed
Rewasvat <fernando_aluani@hotmail.com>
2012-06-20 15:06:30 -0300

    enabled line smoothing besides polygon smoothing

--- MAC420_ComputacaoGrafica/EP2/src/engine/engine.cc
+++ MAC420_ComputacaoGrafica/EP2/src/engine/engine.cc
@@ -54,6 +54,7 @@ void Engine::Initialize(int argc, char* argv[]) {
 	//glDepthMask(GL_TRUE);
 	glEnable(GL_LIGHTING);
 	glEnable(GL_TEXTURE_2D);
+	glEnable(GL_LINE_SMOOTH);
 	glEnable(GL_POLYGON_SMOOTH);
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
