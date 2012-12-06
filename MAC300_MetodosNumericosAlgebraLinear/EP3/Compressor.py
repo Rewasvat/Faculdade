459339801fe84e3e72285a817ccffdb8ed069db4
Rewasvat <fernando_aluani@hotmail.com>
2012-12-06 00:12:41 -0200

    coloquei o segundo householder mas ainda nao funciona

--- MAC300_MetodosNumericosAlgebraLinear/EP3/Compressor.py
+++ MAC300_MetodosNumericosAlgebraLinear/EP3/Compressor.py
@@ -20,13 +20,6 @@ def GetBasisVector(index, size, columnVector=True):
         return np.asmatrix(v).T
     else:
         return np.asmatrix(v)
-
-def randM(w,h):
-    import random
-    m = np.asmatrix( np.zeros( (w,h) ))
-    for i, j in [(a, b) for a in xrange(w) for b in xrange(h)]:
-        m[i,j] = 20 * random.random()
-    return m
     
 def removeNearZeroentries(M):
     for i, j in [(a, b) for a in xrange(M.shape[0]) for b in xrange(M.shape[1])]:
@@ -67,7 +60,7 @@ class Compressor:
             # U = U - 2*(U*Uk)*Uk.T
             U[0:m, k:n] = U[0:m, k:n] - 2*(U[0:m, k:n]*Uk)*Uk.T
             ###
-            if False:#k <= n-2:
+            if k <= n-2:
                 A = A.T
                 print "running right"
                 x = np.asmatrix(np.copy(A[k+1:m, k]))
@@ -80,23 +73,24 @@ class Compressor:
                 V[k+1:m, k:n] = V[k+1:m, k:n] - 2*Vk*(Vk.T*V[k+1:m, k:n])
                 #V[k:m, k+1:n] = V[k:m, k+1:n] - 2*(V[k:m, k+1:n]*Vk)*Vk.T
                 A = A.T
-                #continue
+                ###############################
                 #x = A[k, k+1:n]
                 #Vk = x + np.sign(x[0,0])*norm(x)*GetBasisVector(0, len(x), False)
                 #Vk = Vk / norm(Vk)
                 #Vk = Vk.T
-                #print A
+                
                 ##P = matrix householder de Vk  ( P = I - 2v(vT) )
                 ## A = A * P
                 ## A = A * (I - 2*Vk*Vk.T)
                 ## A = A - 2*(A*Vk)*Vk.T
                 #A[k:m, k+1:n] = A[k:m, k+1:n] - 2*(A[k:m, k+1:n]*Vk)*Vk.T  #check: no algoritmo era 2*(M*Vk)*Vk.T
-                #print A
+                
                 ## V = P * V
                 ## V = (I - 2*Vk*Vk.T)*V
                 ## V = V - 2*Vk*(Vk.T*V)
                 ##V[k:m, k+1:n] = V[k:m, k+1:n] - 2*Vk*(Vk.T*V[k:m, k+1:n])
-                #V[k:m, k+1:n] = V[k:m, k+1:n] - 2*(V[k:m, k+1:n]*Vk)*Vk.T
+                #V[k+1:m, k:n] = V[k+1:m, k:n] - 2*Vk*(Vk.T*V[k+1:m, k:n])
+                ##V[k:m, k+1:n] = V[k:m, k+1:n] - 2*(V[k:m, k+1:n]*Vk)*Vk.T
                 ####
             removeNearZeroentries(A)
             
@@ -117,3 +111,29 @@ class Compressor:
         # Fix U and V shape to match size of S (RANKxRANK)
         # Return compressed image = USVt
         pass
+        
+        
+        
+########################3
+# DEBUG STUFF - REMOVE IT
+
+def randM(w,h):
+    import random
+    m = np.asmatrix( np.zeros( (w,h) ))
+    for i, j in [(a, b) for a in xrange(w) for b in xrange(h)]:
+        m[i,j] = 20 * random.random()
+    return m
+    
+def t(mmm,nnn):
+    matriz = randM(mmm,nnn)
+    comp = Compressor(0)
+    u,b,v = comp.executeGolubKahan(matriz)
+    return matriz,u,b,v
+    
+def t2(mmmm,nnnn):
+    m,u,b,v = t(mmmm,nnnn)
+    print "------ M ------"
+    print m
+    print "------ UBVt ------"
+    print u*b*v.T
+    return str(m) == str(u*b*v.T)
